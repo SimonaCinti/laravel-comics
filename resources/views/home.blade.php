2aef31bee2f18d0abf07acad3022e60c5a24893a
Simona Cinti <simona.cinti2@gmail.com>
2021-01-07 17:07:37 +0100

    basic css done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,24 @@
 @extends('layouts.main')
 
 @section('content')
-    <main class="wrap-home">
+    <main class="wrap-home mt-1">
         <section class="hero">
             <div class="container">
                 <img src="{{ asset('img/cover-home.jpg') }}" alt="cover-hero">
+            </div>
+        </section>
+        <section class="comics mt-2 mb-2">
+            <div class="container">
+                <ul class="comic-list">
+                    @foreach ($comics as $comic)
+                        <li>
+                            <a href="">
+                                <img src="{{$comic['image']}}" alt="cover comic">
+                                <h3>{{$comic['title']}}</h3>
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
         </section>
     </main>
