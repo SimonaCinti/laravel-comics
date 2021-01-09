6fc7e0ef625e7f38e8b8f898fd0ba3dd4f07e384
Simona Cinti <simona.cinti2@gmail.com>
2021-01-09 11:34:42 +0100

    hero and comics

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,11 +2,13 @@
 
 @section('content')
     <main class="wrap-home mt-1">
+        {{-- HERO --}}
         <section class="hero" style="background-image: url({{ $comic['image-hero'] }})">
             <div class="container">
                 <img src="{{ $comic['image-cover'] }}" alt="{{ $comic['title'] }}">
             </div>
         </section>
+        {{-- COMIC DETAIL --}}
         <section class="detail-comic mt-2 mb-2">
             <div class="container">
                 <h1>{{ $comic['title'] }}</h1>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,17 +6,18 @@
         <section class="hero">
             <div class="container">
                 <img src="{{ asset('img/cover-home.jpg') }}" alt="cover-hero">
+                <h2>Current Series</h2>
             </div>
         </section>
         {{-- Comic Gallery --}}
-        <section class="comics mt-2 mb-2">
+        <section class="comics pt-1">
             <div class="container">
                 <ul class="comic-list">
                     @foreach ($comics as $comic)
                         <li>
                             <a href="{{ route('comic-detail', $comic['slug']) }}">
                                 <img src="{{$comic['image']}}" alt="cover comic">
-                                <h3>{{$comic['title']}}</h3>
+                                <h3 class="pt-1 pb-2">{{$comic['title']}}</h3>
                             </a>
                         </li>
                     @endforeach
